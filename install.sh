fe9b81573431b59755008011ec50a1e0c10c3360
Kye Davey <kye.j.davey@gmail.com>
2025-06-03 23:08:29 +1000

    add wallpaper

--- install.sh
+++ install.sh
@@ -39,20 +39,34 @@ sudo systemctl enable gdm.service
 
 # Configure Gnome
 
-# Set gnome bar apps
+## Set gnome bar apps
 gsettings set org.gnome.shell favorite-apps "['Alacritty.desktop', 'google-chrome.desktop',  'code.desktop']"
 
-## Fixed number of workspaces - 6
+## Set Wallpaper
+BACKGROUND_ORG_PATH="$HOME/code/arch-setup/assets/background.jpg"
+BACKGROUND_DEST_DIR="$HOME/.local/share/backgrounds"
+BACKGROUND_DEST_PATH="$BACKGROUND_DEST_DIR/background.jpg"
+
+if [ ! -d "$BACKGROUND_DEST_DIR" ]; then mkdir -p "$BACKGROUND_DEST_DIR"; fi
+
+[ ! -f $BACKGROUND_DEST_PATH ] && cp $BACKGROUND_ORG_PATH $BACKGROUND_DEST_PATH
+gsettings set org.gnome.desktop.background picture-uri $BACKGROUND_DEST_PATH
+gsettings set org.gnome.desktop.background picture-uri-dark $BACKGROUND_DEST_PATH
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+
+## Set Keybinds
+
+### Fixed number of workspaces - 6
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
-# Full-screen with title/navigation bar
+### Full-screen with title/navigation bar
 gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift>F11']"
 
-# Close Window
+### Close Window
 gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
 
-## Use alt for pinned apps
+### Use alt for pinned apps
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
 gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
 gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
@@ -63,7 +77,7 @@ gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
 gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
 gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
 
-# Use super for workspaces
+### Use super for workspaces
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
@@ -71,7 +85,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
 
-# Use shift super for move to workspaces
+### Use shift super for move to workspaces
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Shift><Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Shift><Super>2']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Super>3']"
@@ -79,15 +93,15 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Su
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Shift><Super>5']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Shift><Super>6']"
 
-# Reserve slots for custom keybindings
+### Reserve slots for custom keybindings
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
 
-# Start a new alacritty window (rather than just switch to the already open one)
+### Start a new alacritty window (rather than just switch to the already open one)
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'New Alacritty Window'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'alacritty'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Shift><Alt>1'
 
-# Start a new Chrome window (rather than just switch to the already open one)
+### Start a new Chrome window (rather than just switch to the already open one)
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'New Chrome Window'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'google-chrome'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Shift><Alt>2'
